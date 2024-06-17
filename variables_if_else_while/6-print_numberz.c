b0135e9c5eec0c10902c981e6721e412457b6328
zahraiva <zaraiv06@gmail.com>
2024-06-17 12:23:35 +0000

    save the planet

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
-V
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 48; a < 58 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
