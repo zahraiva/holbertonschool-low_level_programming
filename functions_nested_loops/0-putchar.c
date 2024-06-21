33f398d10adfb0d382a9c3cd4488669a0b28795f
zahraiva <zaraiv06@gmail.com>
2024-06-21 12:39:28 +0000

    save the planet

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
-int main() {
-  printf("_putchar\n");
-  return 0;
-}
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "_putchar\n";
+
+	int b = 0;
+
+	while (a[b] != '\0')
+	{
+		_putchar(a[b]);
+		b++;
+	}
+	return (0);
+}}
