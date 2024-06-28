d82a23bb39d73853a708abe6a18034f1b4450d93
zahraiva <zaraiv06@gmail.com>
2024-06-28 18:51:58 +0000

    save the planet

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - string copy
+ * @dest: dest
+ * @src: src
+ *
+ * Return: return
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = 0;
+	return (dest);
+}
