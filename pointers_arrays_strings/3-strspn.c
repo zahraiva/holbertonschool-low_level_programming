31cf9a8f397f780532aa3ed0e4afa38775dde92b
zahraiva <zaraiv06@gmail.com>
2024-07-06 15:46:59 +0000

    save the planet

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,9 +7,10 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i, j;
+unsigned int count = 0;
+int i, j;
     for (i = 0; s[i] != '\0'; i++)
+
     {
         for (j = 0; accept[j] != '\0'; j++)
         {
