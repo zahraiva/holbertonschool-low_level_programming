f1e95d4b4833d6092a24e0077df52fcf6995fbda
zahraiva <zaraiv06@gmail.com>
2024-07-06 16:24:44 +0000

    save the planet

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,24 @@
-#include "main.h"
+#include <stddef.h>
 /**
- * _strpbrk - check the code
- * @s: - s
- * @accept: - var
- * Return: Always 0.
+ * _strpbrk - strpbrk
+ * @s: source
+ * @accept: accept
+ * Return: modified string or null
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i;
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-if (accept[i] == *s)
-{
-return (s);
-}
-}
-s++;
-}
-return (NULL);
+	int i = 0;
+
+	while (*(s) != '\0')
+	{
+		while (*(accept + i) != '\0')
+		{
+			if (*(s) == *(accept + i))
+				return (s);
+			i++;
+		}
+		s++;
+		i = 0;
+	}
+	return (NULL);
 }
