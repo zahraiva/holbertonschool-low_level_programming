e335d8be8f7d8fbabb39dc61640a86ec444a21b9
zahraiva <zaraiv06@gmail.com>
2024-07-06 20:18:20 +0000

    save the planet

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
