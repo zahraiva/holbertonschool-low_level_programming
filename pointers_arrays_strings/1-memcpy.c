3b0a9342cb68c330403c6455c3bb3ec0bc344976
zahraiva <zaraiv06@gmail.com>
2024-07-06 13:33:03 +0000

    save the planet

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - check the code
+ *  @dest: - input of function
+ *  @src: src
+ *  @n: - var
+ *  Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
