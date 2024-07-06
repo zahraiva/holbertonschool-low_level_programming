c2583771a9b44e98ee66205a02c73678e3c121d0
zahraiva <zaraiv06@gmail.com>
2024-07-06 20:20:03 +0000

    save the planet

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: number of argv
+ * @argv: vector
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
