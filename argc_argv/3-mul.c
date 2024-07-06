290bb69a0e2c49473bc709322778359e0f3d4ece
zahraiva <zaraiv06@gmail.com>
2024-07-06 20:23:04 +0000

    save the planet

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
