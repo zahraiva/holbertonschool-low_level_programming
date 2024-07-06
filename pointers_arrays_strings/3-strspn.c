a29ca1358db53e3019dd09c737f5b768dc6c50e2
zahraiva <zaraiv06@gmail.com>
2024-07-06 15:23:17 +0000

    save the planet

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - check the code
+ * @s: - s
+ * @accept: - accept
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int count = 0;
+    int i, j;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                count++;
+                break;
+            }
+        }
+        if (accept[j] == '\0')
+        {
+            break;
+        }
+    }
+    return (count);
+}
