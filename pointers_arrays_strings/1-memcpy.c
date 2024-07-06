073a3a739bd9e61b29f66a5f384fdb39bfba7356
zahraiva <zaraiv06@gmail.com>
2024-07-06 15:52:54 +0000

    save the planet

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,34 +1,19 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * _memcpy - copies memory area
- * @dest: checker
- * @src: checker
- * @n: checker
- *
+ * _memcpy - check the code
+ * @dest: - input of function
+ * @src: - src
+ * @n: - var
  * Return: Always 0.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int size2 = strlen(src), j;
+    unsigned int i;
 
-	if (n >= size2)
-	{
-		for (j = 0 ; j <= size2 ; j++)
-		{
-			*(dest + j) = *(src + j);
-		}
+    for (i = 0; i < n; i++)
+    {
+        *(dest + i) = *(src + i);
+    }
 
-		return (dest);
-	}
-	else
-	{
-		for (j = 0 ; j < n ; j++)
-		{
-			*(dest + j) = *(src + j);
-		}
-
-		return (dest);
-	}
+    return dest;
 }
