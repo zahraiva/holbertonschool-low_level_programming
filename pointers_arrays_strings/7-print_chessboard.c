2bd0e482e5b6a46990ee5404dcb917cfec94aeba
zahraiva <zaraiv06@gmail.com>
2024-07-06 16:31:15 +0000

    save the planet

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code
+ *
+ * @a: 'var'
+ *
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
