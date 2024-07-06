71a1041b36bce04c0163ca93afacc5ba55d539ef
zahraiva <zaraiv06@gmail.com>
2024-07-06 19:58:17 +0000

    save the planet

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check the code
+ * @s: - s
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
