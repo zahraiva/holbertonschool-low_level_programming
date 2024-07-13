5d56b48c5c44ad4f4ab4218ed1d3cd13d8b1c9c9
zahraiva <zaraiv06@gmail.com>
2024-07-13 13:41:50 +0000

    save the planet

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <limits.h>
-
 /**
- * 
  * malloc_checked - prototype for allocates memory using malloc
  * @b: - b
- * Return: Always 0.
+ *
+ * Return: always 0
  */
 void *malloc_checked(unsigned int b)
 {
