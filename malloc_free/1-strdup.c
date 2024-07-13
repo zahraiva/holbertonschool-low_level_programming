a021179abb6c7a931e3aaf383a35680761318861
zahraiva <zaraiv06@gmail.com>
2024-07-13 11:39:32 +0000

    save the planet

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - strdup
+ *
+ * @str - str
+ *
+ * Return: Always 0
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i = 0, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	array = malloc((len + 1) * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
