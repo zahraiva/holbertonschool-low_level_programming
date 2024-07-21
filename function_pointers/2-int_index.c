2cf2b6aefdc691ca5d9bd23acbd3a932458e896b
zahraiva <zaraiv06@gmail.com>
2024-07-21 10:05:23 +0000

    save the planet

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function for searches for an integer
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer
+ *
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
