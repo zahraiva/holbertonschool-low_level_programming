34589afb67656474a18025b39ae81d7e35082dec
zahraiva <zaraiv06@gmail.com>
2024-07-21 09:58:54 +0000

    save the planet

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: name of pointer
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
