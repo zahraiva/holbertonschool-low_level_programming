f14b62c28eaeb63366ba34ba4e2a6c4f4a4bbf76
Your Name <you@example.com>
2024-08-02 07:30:30 +0000

    count of nodes

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_list - Main
+ * @h: Clone of struct
+ * Return: Count of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+
+}
