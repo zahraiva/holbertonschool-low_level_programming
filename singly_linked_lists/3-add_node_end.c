6f44f44cc7c34c1585f76cf2def090362f4b67f8
Your Name <you@example.com>
2024-08-02 07:51:45 +0000

    fixed code

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,51 +1,36 @@
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - main
- * @head: of the linked list
- * @str: var1
- * Return: linked list
- */
-
+  * add_node_end - add new node to list
+  * @head: start of list
+  * @str: new data
+  * Return: updated list
+  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int length_nodesdata = 0, index = 0;
-	char *duplicate_str = strdup(str);
-	list_t *t1node = (list_t *) malloc(sizeof(list_t));
-	list_t *t2node;
+	list_t *temp, *tmp;
+	int len = 0;
 
-	if (duplicate_str == NULL)
-	{
-		free(th1node);
+	while (str[len] != '\0')
+		len++;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
 		return (NULL);
-	}
-	if (t1node == NULL)
-		return (NULL);
-	while (str[index] != '\0')
+	temp->str = strdup(str);
+	temp->len = len;
+	temp->next = NULL;
+	tmp = *head;
+	if (*head)
 	{
-		length_nodesdata++;
-		index++;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = temp;
 	}
-
-	t1node->str = duplicate_str;
-	t1node->len = length_nodesdata;
-	t1node->next = NULL;
-
-	if (*head == NULL)
-		*head = t1node;
 	else
-	{
-		*head = t2node;
-
-		while (t2node->next != NULL)
-		{
-			t2node = t2node->next;
-		}
-		t2node->next = t1node;
-	}
-	return (*head);
-
+		*head = temp;
+	return (temp);
 }
