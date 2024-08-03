1232c5cdd310358e6534495bcfc7d9679e9d1d9f
Your Name <you@example.com>
2024-08-03 12:29:14 +0000

    6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - main
+ * head node
+ * Return: s
+ */
+
+
+int sum_dlistint(dlistint_t *head)
+{
+	int s = 0;
+
+	while (head != NULL)
+	{
+		s += head->n;
+		head = head->next;
+	}
+	return (s);
+}
