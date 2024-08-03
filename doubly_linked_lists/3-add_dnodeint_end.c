1a2c63abc8bd737ab29f333ef3500f7fcea4e272
Your Name <you@example.com>
2024-08-03 12:16:27 +0000

    3 task

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node
+ * @head: head node
+ * @n: valuse
+ * Return: size_t
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *buf;
+	dlistint_t *temp = *head;
+
+	node = malloc(sizeof(dlistint_t));
+	if (buf == NULL)
+		return (NULL);
+	buf->n = n;
+	buf->next = NULL;
+	if (*head == NULL)
+	{
+		buf->prev = NULL;
+		*head = buf;
+		return (buf);
+	}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+		temp->next = buf;
+		buf->prev = temp;
+	return (buf);
+}
