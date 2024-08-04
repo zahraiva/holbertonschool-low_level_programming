c35e6a3887210ef0fdabc63b9085d5d5940e2717
Your Name <you@example.com>
2024-08-04 14:26:01 +0000

    1 task

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of a file
+ * @text_content: content to write
+ * Return: num of letters
+ * 
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		len = strlen(text_content);
+		if (write(fd, text_content, len) == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
