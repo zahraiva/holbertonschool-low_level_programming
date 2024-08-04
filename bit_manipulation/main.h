87d1715ebd96abd36eca6477ed29590cf8605040
Your Name <you@example.com>
2024-08-04 12:48:05 +0000

    fix

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 
-insigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
